f43a3b5456f1b1cd8aa3b9110d391f96ed1b6d84
public-abstract-void <148276296+public-abstract-void@users.noreply.github.com>
2026-01-12 11:38:45 -0800

    Update spell.js
    fix #2

--- spell.js
+++ spell.js
@@ -1,19 +1,17 @@
-<script>
-   function heavyLoop() {
-     let x = 0;
-     for (let i = 0; i < 5e7; i++) x = (x + i) | 0;
-     return x;
-   }
+function heavyLoop() {
+  let x = 0;
+  for (let i = 0; i < 5e7; i++) x = (x + i) | 0;
+  return x;
+}
 
-   function castSpell() {
-     let t1 = performance.now();
-     heavyLoop();
-     let t2 = performance.now();
-     heavyLoop();
-     let t3 = performance.now();
+function castSpell() {
+  let t1 = performance.now();
+  heavyLoop();
+  let t2 = performance.now();
+  heavyLoop();
+  let t3 = performance.now();
 
-     document.getElementById("spellResult").innerText =
-       "First cast: " + (t2 - t1).toFixed(1) + " ms\n" +
-       "Second cast: " + (t3 - t2).toFixed(1) + " ms";
-   }
-</script>
+  document.getElementById("spellResult").innerText =
+    "First cast: " + (t2 - t1).toFixed(1) + " ms\n" +
+    "Second cast: " + (t3 - t2).toFixed(1) + " ms";
+}
